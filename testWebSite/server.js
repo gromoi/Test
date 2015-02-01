8bdb91a1b9fbc84257c15039baee5100c2ecd1eb
gromoi <ivanivgrom@gmail.com>
2015-02-01 19:54:07 +0300

    Add simple server

---
+++ testWebSite/server.js
@@ -0,0 +1,24 @@
+/**
+ * Created by gromoi on 2/1/15.
+ */
+
+var Hapi = require('hapi');
+
+// Create a server with a host and port
+var server = new Hapi.Server();
+server.connection({
+    host: 'localhost',
+    port: 8000
+});
+
+// Add the route
+server.route({
+    method: 'GET',
+    path:'/hello',
+    handler: function (request, reply) {
+        reply('hello world');
+    }
+});
+
+// Start the server
+server.start();
